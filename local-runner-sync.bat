b0d07d78e0c6fd8af2ee62679ae2788798383177
SladeThe <sladethe@gmail.com>
2016-11-21 17:03:54 +0300

    Improved launch scripts for Windows; fixed bug with angle ranges when playing for Renegades; fixed bug with collision of minions and bonuses; few minor improvements.

--- local-runner-sync.bat
+++ local-runner-sync.bat
@@ -1,1 +1,18 @@
-start javaw -Xms512m -Xmx1G -jar "local-runner.jar" local-runner-sync.properties local-runner-sync.default.properties
+set JAVA_BIN="
+
+if "%JAVA8_64_HOME%" neq "" (
+    if exist "%JAVA8_64_HOME%\bin\javaw.exe" (
+        set JAVA_BIN="%JAVA8_64_HOME%\bin\"
+        goto java-start
+    )
+)
+
+if "%JAVA_HOME%" neq "" (
+    if exist "%JAVA_HOME%\bin\javaw.exe" (
+        set JAVA_BIN="%JAVA_HOME%\bin\"
+        goto java-start
+    )
+)
+
+:java-start
+start "" "%JAVA_BIN:"=%javaw" -Xms512m -Xmx1G -jar "local-runner.jar" local-runner-sync.properties local-runner-sync.default.properties
--- local-runner.bat
+++ local-runner.bat
@@ -1,1 +1,18 @@
-start javaw -Xms512m -Xmx1G -XX:+UseConcMarkSweepGC -jar "local-runner.jar" local-runner.properties local-runner.default.properties
+set JAVA_BIN="
+
+if "%JAVA8_64_HOME%" neq "" (
+    if exist "%JAVA8_64_HOME%\bin\javaw.exe" (
+        set JAVA_BIN="%JAVA8_64_HOME%\bin\"
+        goto java-start
+    )
+)
+
+if "%JAVA_HOME%" neq "" (
+    if exist "%JAVA_HOME%\bin\javaw.exe" (
+        set JAVA_BIN="%JAVA_HOME%\bin\"
+        goto java-start
+    )
+)
+
+:java-start
+start "" "%JAVA_BIN:"=%javaw" -Xms512m -Xmx1G -XX:+UseConcMarkSweepGC -jar "local-runner.jar" local-runner.properties local-runner.default.properties
